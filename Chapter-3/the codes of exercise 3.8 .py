b9ca211ca2255cdbcb52054703ea8084974b0872
luokaifa-whu <luokaif@qq.com>
2016-06-10 23:00:24 +0800

    Update the codes of exercise 3.8 .py

--- Chapter-3/the codes of exercise 3.8 .py
+++ Chapter-3/the codes of exercise 3.8 .py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun May 30 10:42:04 2016
+Created on Fri Jun 10 21:01:08 2016
 
 @author: luokaifa
 """
@@ -10,13 +10,12 @@ import math     # import packages
 import matplotlib.pyplot as plt
 
 #设置变量及其初值
-q=0.0
-Omega_D=2.0
-F_D=0.2
+q=0.5
+Omega_D=2.0/3.0
+F_D=50.0
 g=9.8
-l=1.0
-i = 0.1
-t = 0.001    #t为步长！
+l=9.8
+t = 0.01    #t为步长！
 temp_omega = 0.
 temp_theta = 0.
 
@@ -30,17 +29,21 @@ t_theta.append(0.0)
 
 
 #循环，也就是差分方程
-for i in range(0,1000):
-    temp_omega = omega[i]-g/l*math.sin(theta[i])*(t*i)
+for i in range(0,50000):
+    temp_omega = omega[i]+(-g/l*math.sin(theta[i])-q*omega[i]+F_D*math.sin(Omega_D*t*i))*t
     omega.append(temp_omega)
     temp_theta = omega[i+1]*t+theta[i]
     theta.append(temp_theta)
     t_theta.append(t*i)
+    if theta[i+1] > math.pi:
+        theta[i+1] = theta[i+1] - 2*math.pi
+    if theta[i+1] < -math.pi:
+        theta[i+1] = theta[i+1] + 2*math.pi
     
 #绘图部分
-plt.figure(figsize = [10,6])
-plt.plot(theta,omega, 'r')           #这里是绘制的相位图
-plt.ylabel('omega(radians/s)')
-plt.xlabel('theta(radians)')      
-plt.ylim(-20,20)
-plt.title('The Nonlinear Pendulum')
+plt.figure(figsize = [8,6])
+plt.scatter(theta,omega,c='r',s=0.1)           #这里是绘制的相位图
+plt.xlabel('theta(radians)')
+plt.ylabel('omega(radians/s)')     
+plt.legend({'omega versus theta  F_D=50.0'},loc='upper center')
+plt.title('The Realistic Pendulum')
